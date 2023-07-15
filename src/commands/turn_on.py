57b64382d6dcb3d6c0ce50cd0e97ecbefe480853
Bruno Piaggio <brunopiaggiok@gmail.com>
2023-07-15 19:42:26 -0300

    Fix turn_on validations

--- src/commands/turn_on.py
+++ src/commands/turn_on.py
@@ -20,10 +20,13 @@ class TurnOn(Command):
         self.validator = Draft7Validator(arguments_schema)
 
     def validate_arguments(self):
+        if self.args is None:
+            return
+
         errors = [e for e in self.validator.iter_errors(self.args)]
         if len(errors) > 0:
             raise ParseError(errors)
 
     def exec(self):
-        section_id = self.args['section_id'] if 'section_id' in self.args else None
+        section_id = self.args['section_id'] if self.args is not None and 'section_id' in self.args else None
         self.controller.turn_on(section_id)
