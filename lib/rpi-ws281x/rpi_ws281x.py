1e98a847da3a1dc4a40cfd23fdc6ec79c42d6f7e
Bruno Piaggio <brunopiaggiok@gmail.com>
2024-03-05 21:58:18 -0300

    Implement missed methods in mocked lib rpi_ws281x

--- lib/rpi-ws281x/rpi_ws281x.py
+++ lib/rpi-ws281x/rpi_ws281x.py
@@ -1,5 +1,21 @@
-class PixelStrip:
-  pass
+import logging
+
+_logger = logging.getLogger(__name__)
+
 
 class Color:
-  pass
+  def __init__(self, r, g, b):
+    pass
+
+class PixelStrip:
+  def __init__(self, n, pin, freq_hz, dma, invert, brightness, channel):
+    pass
+
+  def begin(self):
+    _logger.info('Initializing strip ...')
+  
+  def setPixelColor(self, index: int, color: Color):
+    pass
+
+  def show(self):
+    _logger.info('Displaying light colors ...')
