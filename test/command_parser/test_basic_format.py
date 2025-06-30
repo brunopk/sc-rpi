c5a3db2688c2ce3ae5151c2aee291722fc360b79
Bruno Piaggio <brunopiaggiok@gmail.com>
2025-06-30 20:16:27 -0300

    New test file

---
+++ test/command_parser/test_basic_format.py
@@ -0,0 +1,60 @@
+"""Test basic format (JSON format requirements) for all commands.
+
+This tests do not validate specific command arguments.
+"""
+
+import logging
+from configparser import ConfigParser
+from unittest import TestCase
+
+from command_parser import CommandParser
+from controllers import HardwareController
+
+
+class TestCreatingSections(TestCase):
+    """Test basic format (JSON format requirements) for all commands.
+
+    This tests do not validate specific command arguments.
+    """
+
+    def setUp(self) -> None:
+        """Set required configurations before running each test case."""
+        config = ConfigParser()
+        config.read("./config.ini")
+        logging.basicConfig(level=None)
+        hw_controller = HardwareController(config)
+        self.parser = CommandParser(hw_controller)
+
+    def test_disconnect_basic_format(self) -> None:
+        """Test case test_disconnect_basic_format."""
+        self.parser.parse('{"name": "disconnect"}')
+
+    def test_reset_basic_format(self) -> None:
+        """Test case test_reset_basic_format."""
+        self.parser.parse('{"name": "reset"}')
+
+    def test_section_add_basic_format(self) -> None:
+        """Test case test_section_add_basic_format."""
+        self.parser.parse('{"name": "section_add"}')
+
+    def test_section_edit_basic_format(self) -> None:
+        """Test case test_section_edit_basic_format."""
+        self.parser.parse('{"name": "section_edit"}')
+
+    def test_section_remove_basic_format(self) -> None:
+        """Test case test_section_remove_basic_format."""
+        self.parser.parse('{"name": "section_remove"}')
+
+    def test_status_basic_format(self) -> None:
+        """Test case test_status_basic_format."""
+        self.parser.parse('{"name": "status"}')
+
+    def test_turn_on_basic_format(self) -> None:
+        """Test case test_turn_on_basic_format."""
+        self.parser.parse('{"name": "turn_on"}')
+
+    def test_turn_off_basic_format(self) -> None:
+        """Test case test_turn_off_basic_format."""
+        self.parser.parse('{"name": "turn_off"}')
+
+
