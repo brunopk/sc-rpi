9a71f7d2fff5552b1bb6b956a2961eb6e8448a20
Bruno Piaggio <brunopiaggiok@gmail.com>
2025-08-19 19:43:45 -0300

    Improve test_reset_cmd.py

--- test/commands/test_reset_cmd.py
+++ test/commands/test_reset_cmd.py
@@ -1,7 +1,6 @@
 """Contains the `TestReset` class."""
 
 import logging
-from dataclasses import asdict
 from unittest import TestCase
 
 from sc_rpi.commands.reset import ResetCmd
@@ -34,5 +33,5 @@ class TestReset(TestCase):
         resp = cmd.run()
 
         self.assertIsInstance(resp, Response)
-        self.assertIsInstance(asdict(resp), dict)
+        self.assertIsInstance(resp.to_json(), str)
         self.assertEqual(resp.command_name, ResetCmd.name)
